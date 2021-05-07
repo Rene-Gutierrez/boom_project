983b4ac17b82450cc567d21d44c7c565cf22e44b
Rene Gutierrez Marquez <rgutie17@ucsc.edu>
2021-05-07 16:38:21 -0500

    U_boom_iteration simplified w Fail

--- u_boom_iteration.R
+++ u_boom_iteration.R
@@ -37,35 +37,22 @@ u_boom_iteration <- function(y,
   B     <- matrix(data = b[1:(V * P)], nrow = V, ncol = P)
   Theta <- diag(P)
   Theta[upper.tri(Theta, diag = TRUE)] <- 0
-  Theta[lower.tri(Theta)] <- b[(V * P + 1):(V * P + P * (P - 1) / 2)]
+  vT <- b[(V * P + 1):(V * P + P * (P - 1) / 2)]
+  Theta[lower.tri(Theta)] <- vT
   Theta <- Theta + t(Theta)
   #############################################################################
   # Horse Shoe Structure for B
   #############################################################################
   # Samples l2
-  for(i in 1:P){
-    if(g[i] == 1){
-      l2[, i] <- 1 / rgamma(n     = V,
-                            shape = 1,
-                            rate  = 1 / v[, i] + B[, i]^2 / (2 * s2 * t2))
-    } else {
-      l2[, i] <- 1 / rgamma(n     = V,
-                            shape = 1 / 2,
-                            rate  = 1 / v[, i])
-    }
-  }
+  l2 <- 1 / rgamma(n     = V * P,
+                   shape = 1,
+                   rate  = 1 / c(v) + c(B)^2 / (2 * s2 * t2))
+  l2 <- matrix(data = l2, nrow = V, ncol = P)
   
   # Samples t2
-  if(sum(g) > 0){
-    t2 <- 1 / rgamma(n     = 1,
-                     shape = (sum(g) * V + 1) / 2,
-                     rate  = 1 / xi +
-                       sum(Lambda(M = B^2, g = g, a = 0) / (2 * s2 * l2)))
-  } else {
-    t2 <- 1 / rgamma(n     = 1,
-                     shape = 1 / 2,
-                     rate  = 1 / xi)
-  }
+  t2 <- 1 / rgamma(n     = 1,
+                   shape = (P * V + 1) / 2,
+                   rate  = 1 / xi + sum(B^2 / (2 * s2 * l2)))
   
   # Samples v2
   v  <- 1 / rgamma(n     = P * V,
@@ -83,44 +70,23 @@ u_boom_iteration <- function(y,
   # Horse Shoe Structure for Theta
   #############################################################################
   # Samples m2
-  for(i in 2:P){
-    for(j in 1:(i - 1)){
-      if(g[i] * g[j] == 1){
-        m2[i, j] <- 1 / rgamma(n     = 1,
-                               shape = 1,
-                               rate  = 1 / w[i, j] + Theta[i, j]^2 / (2 * s2 * r2))
-        m2[j, i] <- m2[i, j] 
-      } else {
-        m2[i, j] <- 1 / rgamma(n     = 1,
-                               shape = 1 / 2,
-                               rate  = 1 / w[i, j])
-        m2[j, i] <- m2[i, j]
-      }
-    }
-  }
+  vm2 <- 1 / rgamma(n     = P * (P - 1) / 2,
+                    shape = 1,
+                    rate  = 1 / w[lower.tri(w)] + Theta[lower.tri(Theta)]^2 / (2 * s2 * r2))
+  m2[lower.tri(m2)] <- vm2
+  m2                <- m2 + t(m2)
   
   # Samples r2
-  temp1 <- Theta[g == 1, g == 1]
-  temp1 <- temp1[lower.tri(temp1)]
-  temp2 <- m2[g == 1, g == 1]
-  temp2 <- temp2[lower.tri(temp2)]
-  if(sum(g) > 0){
-    r2 <- 1 / rgamma(n     = 1,
-                     shape = (sum(g) * (sum(g) - 1) / 2 + 1) / 2,
-                     rate  = 1 / nu +
-                       sum(temp1^2 / (2 * s2 * temp2)))
-  } else {
-    r2 <- 1 / rgamma(n     = 1,
-                     shape = 1 / 2,
-                     rate  = 1 / nu)
-  }
+  r2 <- 1 / rgamma(n     = 1,
+                   shape = (P * (P - 1) / 2 + 1) / 2,
+                   rate  = 1 / nu +
+                     sum(Theta[lower.tri(Theta)]^2 / (2 * s2 * m2[lower.tri(m2)])))
   
   # Samples w
-  out <- 1 / rgamma(n     = P * (P - 1) / 2,
-                    shape = 1,
-                    rate  = 1 + 1 / m2[lower.tri(m2)])
-  diag(w)         <- 0
-  w[lower.tri(w)] <- out
+  vw <- 1 / rgamma(n     = P * (P - 1) / 2,
+                   shape = 1,
+                   rate  = 1 + 1 / m2[lower.tri(m2)])
+  w[lower.tri(w)] <- vw
   w               <- w + t(w)
   
   # Samples nu
