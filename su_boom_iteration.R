89d4f014f7cfe329c2603b2c6e1863a81036223a
Rene Gutierrez Marquez <rgutie17@ucsc.edu>
2021-05-16 20:41:28 -0500

    su_boom

---
+++ su_boom_iteration.R
@@ -0,0 +1,191 @@
+# Fast Horse Shoe Sampler (For Big p and small n)
+
+su_boom_iteration <- function(y,
+                              X,
+                              t2, # Horseshoe Structure for B
+                              l2, #
+                              xi, #
+                              v,  #
+                              c2, # Spike & Slab Structure for Theta
+                              m2, #
+                              s2,
+                              g,
+                              e2){
+  #############################################################################
+  # Set-Up
+  #############################################################################
+  n <- dim(X)[1]
+  V <- dim(l2)[1]
+  P <- dim(l2)[2]
+  
+  # Numerical Failure Flag
+  flag <- FALSE
+  
+  #############################################################################
+  # Samples B and Theta
+  #############################################################################
+  # Prior Diagonal
+  temp1 <- s2 * c(t2 * t(t(l2) * g + (1 - g) * e2))
+  temp2 <- (c2 * m2 * (g %*% t(g)) + (1 - (g %*% t(g))) * m2)[lower.tri((g %*% t(g)))]
+  Lam   <- c(temp1, temp2)
+  # Fast Sampling
+  b <- fast_sampler(Phi = X / sqrt(s2),
+                    D   = Lam,
+                    a   = y / sqrt(s2))
+  # Distributes the Estimate
+  B     <- matrix(data = b[1:(V * P)], nrow = V, ncol = P)
+  Theta <- diag(P)
+  Theta[upper.tri(Theta, diag = TRUE)] <- 0
+  Theta[lower.tri(Theta)] <- b[(V * P + 1):(V * P + P * (P - 1) / 2)]
+  Theta <- Theta + t(Theta)
+  #############################################################################
+  # Horse Shoe Structure for B
+  #############################################################################
+  # Samples l2
+  for(i in 1:P){
+    if(g[i] == 1){
+      l2[, i] <- 1 / rgamma(n     = V,
+                            shape = 1,
+                            rate  = 1 / v[, i] + B[, i]^2 / (2 * s2 * t2))
+    } else {
+      l2[, i] <- 1 / rgamma(n     = V,
+                            shape = 1 / 2,
+                            rate  = 1 / v[, i])
+    }
+  }
+  
+  # Samples t2
+  if(sum(g) > 0){
+    t2 <- 1 / rgamma(n     = 1,
+                     shape = (sum(g) * V + 1) / 2,
+                     rate  = 1 / xi +
+                       sum(Lambda(M = B^2, g = g, a = 0) / (2 * s2 * l2)))
+  } else {
+    t2 <- 1 / rgamma(n     = 1,
+                     shape = 1 / 2,
+                     rate  = 1 / xi)
+  }
+  
+  # Samples v2
+  v  <- 1 / rgamma(n     = P * V,
+                   shape = 1,
+                   rate  = 1 + 1 / c(l2))
+  v <- matrix(data = v, nrow = V, ncol = P)
+  
+  # Samples xi
+  xi <- 1 / rgamma(n     = 1,
+                   shape = 1,
+                   rate  = 1 + 1 / t2)
+  
+  
+  #############################################################################
+  # Spike & Slab Structure for Theta
+  #############################################################################
+
+  #############################################################################
+  # Samples s2
+  #############################################################################
+  temp1 <- c(t2 * t(t(l2) * g + (1 - g) * e2))
+  Lam   <- c(temp1)
+  s2 <- 1 / rgamma(n     = 1,
+                   shape = (n + P * V) / 2,
+                   rate  = crossprod(x = y - X %*% b) / 2 +
+                     t(c(B)[Lam != 0] / Lam[Lam != 0]) %*% c(B)[Lam != 0] / 2)
+  
+  #############################################################################
+  # Samples g
+  #############################################################################
+  pg <- rep(0, P)
+  for(i in 1:P){
+    # print(i)
+    # print(g)
+    # print('p0')
+    # print(sum(dnorm(x    = B[, i],
+    #             mean = 0,
+    #             sd   = sqrt(s2 * e2),
+    #             log  = TRUE)))
+    # print(sum(dnorm(x    = Theta[i, -i][g[-i] == 1],
+    #                 mean = 0,
+    #                 sd   = sqrt(c2 * m2),
+    #                 log  = TRUE)))
+    # print(dnorm(x    = Theta[i, -i][g[-i] == 1],
+    #             mean = 0,
+    #             sd   = sqrt(s2 * o2),
+    #             log  = TRUE))
+    # print(sum(dnorm(x    = B[, i],
+    #                 mean = 0,
+    #                 sd   = sqrt(s2 * e2),
+    #                 log  = TRUE)) + log(1/2) +
+    #         sum(dnorm(x    = Theta[i, -i][g[-i] == 1],
+    #                   mean = 0,
+    #                   sd   = sqrt(s2 * o2),
+    #                   log  = TRUE)))
+    p0   <- sum(dnorm(x    = B[, i],
+                      mean = 0,
+                      sd   = sqrt(s2 * e2),
+                      log  = TRUE)) + log(1/2) +
+      sum(dnorm(x    = Theta[i, -i][g[-i] == 1],
+                mean = 0,
+                sd   = sqrt(m2),
+                log  = TRUE))
+    # print('p1')
+    # print(sum(dnorm(x    = B[, i],
+    #             mean = 0,
+    #             sd   = sqrt(s2 * t2 * l2[, i]),
+    #             log  = TRUE)))
+    # print(sum(dnorm(x    = Theta[i, -i][g[-i] == 1],
+    #                 mean = 0,
+    #                 sd   = sqrt(s2 * r2 * m2[, i]),
+    #                 log  = TRUE)))
+    # print(dnorm(x    = Theta[i, -i][g[-i] == 1],
+    #             mean = 0,
+    #             sd   = sqrt(s2 * r2 * m2[-i, i][g[-i] == 1]),
+    #             log  = TRUE))
+    # print(sum(dnorm(x    = B[, i],
+    #                 mean = 0,
+    #                 sd   = sqrt(s2 * t2 * l2[, i]),
+    #                 log  = TRUE)) + log(1/2) +
+    #         sum(dnorm(x    = Theta[i, -i][g[-i] == 1],
+    #                   mean = 0,
+    #                   sd   = sqrt(s2 * r2 * m2[, i]),
+    #                   log  = TRUE)))
+    p1   <- sum(dnorm(x    = B[, i],
+                      mean = 0,
+                      sd   = sqrt(s2 * t2 * l2[, i]),
+                      log  = TRUE)) + log(1/2) +
+      sum(dnorm(x    = Theta[i, -i][g[-i] == 1],
+                mean = 0,
+                sd   = sqrt(c2 * m2),
+                log  = TRUE))
+    p1   <- exp(sum(p1) - sum(p0))
+    if(is.finite(p1)){
+      p1 <- p1 / (p1 + 1)
+    } else {
+      p1 <- 1
+    }
+    g[i] <- rbinom(n    = 1,
+                   size = 1,
+                   prob = p1)
+    pg[i] <- p1
+  }
+  # g <- c(rep(1, QQ), rep(0, P - QQ))
+  # g <- rep(1, P)
+  
+  temp1 <- s2 * c(t2 * t(t(l2) * g + (1 - g) * e2))
+  temp2 <- (c2 * m2 * (g %*% t(g)) + m2 * (1 - (g %*% t(g))))[lower.tri((g %*% t(g)))]
+  Lam   <- c(temp1, temp2)
+  #############################################################################
+  # Returns the Samples
+  #############################################################################
+  return(list(B      = B,
+              l2     = l2,
+              v      = v,
+              t2     = t2,
+              xi     = xi,
+              Theta  = Theta,
+              s2     = s2,
+              g      = g,
+              pg     = pg,
+              D      = Lam,
+              flag   = flag))
+} 
---
+++ su_boom_sampler.R
@@ -0,0 +1,131 @@
+### Sampler
+
+su_boom_sampler <- function(A,
+                            G,
+                            y,
+                            S,
+                            c2,
+                            m2,
+                            e2){
+  #############################################################################
+  # Set-Up
+  #############################################################################
+  # Problem Dimensions
+  n <- dim(G)[1]
+  V <- dim(G)[2]
+  P <- dim(G)[3]
+  
+  # Data Pre-processing
+  X1      <- G
+  dim(X1) <- c(n, P * V)
+  X2      <- A
+  dim(X2) <- c(n, P * P)
+  temp    <- matrix(data = NA, nrow = P, ncol = P)
+  X2      <- X2[, c(lower.tri(temp))]
+  X       <- cbind(X1, X2)
+  
+  # Variable Initialization
+  l2       <- matrix(data = 1, nrow = V, ncol = P)
+  v        <- matrix(data = 1, nrow = V, ncol = P)
+  t2       <- 1
+  xi       <- 1
+  s2       <- 1
+  g        <- c(rep(1, QQ), rep(0, P - QQ))
+  
+  # Sample Variables
+  sam_B        <- array(data = NA, dim = c(S, V, P))
+  sam_l2       <- array(data = NA, dim = c(S, V, P))
+  sam_l2_t2_s2 <- array(data = NA, dim = c(S, V, P))
+  sam_l2_t2    <- array(data = NA, dim = c(S, V, P))
+  sam_e2_s2    <- array(data = NA, dim = c(S, V, P))
+  sam_v        <- array(data = NA, dim = c(S, V, P))
+  sam_t2       <- numeric(length = S)
+  sam_xi       <- numeric(length = S)
+  sam_Theta    <- array(data = NA, dim = c(S, P, P))
+  sam_s2       <- numeric(length = S)
+  sam_g        <- matrix(data = NA, nrow = S, ncol = P)
+  sam_pg       <- matrix(data = NA, nrow = S, ncol = P)
+  sam_D        <- matrix(data = NA, nrow = S, ncol = P * V + (P - 1) * P / 2)
+  
+  #############################################################################
+  # Sampling
+  #############################################################################
+  # Progress Bar
+  pb <- txtProgressBar(min     = 0,
+                       max     = 1,
+                       initial = 0,
+                       style   = 3,
+                       width   = 72)
+  
+  # Sampling
+  for(s in 1:S){
+    ###########################################################################
+    # Samples B Structure
+    ###########################################################################
+    # Samples the Horseshoe Structure
+    out <- su_boom_iteration(y  = y,
+                             X  = X,
+                             l2 = l2,
+                             v  = v,
+                             t2 = t2,
+                             xi = xi,
+                             c2 = c2,
+                             m2 = m2,
+                             s2 = s2,
+                             g  = g,
+                             e2 = e2)
+    # Assigns and Distributes the values
+    # B Structure
+    B     <- out$B
+    l2    <- out$l2
+    v     <- out$v
+    t2    <- out$t2
+    xi    <- out$xi
+    # Theta Structure
+    Theta <- out$Theta
+    # Error
+    s2 <- out$s2
+    # Sparsity Structure
+    g  <- out$g
+    pg <- out$pg
+    # Coefficient Priors
+    D  <- out$D
+    
+    # Saves Variables
+    sam_B[s,,]        <- B
+    sam_l2[s,,]       <- l2
+    sam_l2_t2_s2[s,,] <- l2 * t2 * s2
+    sam_l2_t2[s,,]    <- l2 * t2
+    sam_e2_s2[s,,]    <- e2 * s2
+    sam_v[s,,]        <- v
+    sam_t2[s]         <- t2
+    sam_xi[s]         <- xi
+    sam_Theta[s,,]    <- Theta
+    sam_s2[s]         <- s2
+    sam_g[s,]         <- g
+    sam_pg[s,]        <- pg
+    sam_D[s,]         <- D
+    
+    ###########################################################################
+    # Progress Bar Update
+    ###########################################################################
+    setTxtProgressBar(pb    = pb,
+                      value = s / S)
+    
+  }
+  
+  # Returns
+  return(list(B        = sam_B,
+              l2       = sam_l2,
+              l2_t2_s2 = sam_l2_t2_s2,
+              l2_t2    = sam_l2_t2,
+              e2_s2    = sam_e2_s2,
+              v        = sam_v,
+              t2       = sam_t2,
+              xi       = sam_xi,
+              Theta    = sam_Theta,
+              s2       = sam_s2,
+              g        = sam_g,
+              pg       = sam_pg,
+              D        = sam_D))
+}
